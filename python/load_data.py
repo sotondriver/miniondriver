14d3cd1a9f93e5811fcb5c68b7af0dc3a48efa81
harrysocool <harry_so_cool@live.com>
2016-06-02 17:30:33 +0100

    add the load_data

--- python/load_data.py
+++ python/load_data.py
@@ -1,22 +1,22 @@
 import os
 import pandas as pd
+from itertools import izip
 
 
-class order(object):
 
-    def __init__(self):
-        self._order_id = order_id
-        self._driver_id = driver_id
-        self._passenger_id = passenger_id
-        self._district_st_hash = district_st_hash
-        self._district_ed_hash = district_ed_hash
-        self._price = price
-        self._time = time
-        self._weather = weather
-        self._temp = temp
-        self._pm25 = pm25
-
-
+# class order(object):
+#
+#     def __init__(self):
+#         self._order_id = order_id
+#         self._driver_id = driver_id
+#         self._passenger_id = passenger_id
+#         self._district_st_hash = district_st_hash
+#         self._district_ed_hash = district_ed_hash
+#         self._price = price
+#         self._time = time
+#         self._weather = weather
+#         self._temp = temp
+#         self._pm25 = pm25
 
 
 def listdir_no_hidden(path):
@@ -26,3 +26,23 @@ def listdir_no_hidden(path):
             list1.append(f)
     return list1
 
+def load_cluster_map(path):
+    global district_dict
+    table = pd.read_table(path,names=['district hash','district_id'])
+    array = table.get_values()
+    district_dict = {array[i][0]: array[i][1] for i in range(0, len(array), 1)}
+    return district_dict
+
+def load_poi(path, district_dict):
+
+    with open(path) as f:
+        for line in f:
+            line = line.rstrip('\n')
+            linedata = line.split('\t')
+            linedata[0] = district_dict[linedata[0]]
+
+if __name__ == '__main__':
+    p1 = '../season_1/training_data/cluster_map/cluster_map'
+    p2 = '../season_1/training_data/poi_data/poi_data'
+    district_dict = load_cluster_map(p1)
+    load_poi(p2, district_dict)
