d31e73830f16fe1ecb55391a7ba7f9e5caa37fb8
harrysocool <harry_so_cool@live.com>
2016-06-09 23:44:51 +0100

    now add the test.py to choose from multi-model the best one. based on unseen test data

--- python_code/LSTM/test.py
+++ python_code/LSTM/test.py
@@ -4,8 +4,10 @@ Created on 16/6/9 17:28 2016
 
 @author: harry sun
 """
+import linecache
+
 from LSTM import initial_lstm_model
-from extend_function import listdir_no_hidden
+from extend_function import listdir_no_hidden, write_list_to_csv
 from process_data import load_test_data
 import numpy as np
 
@@ -32,7 +34,7 @@ def return_mape(predict_result, true_result):
     mape_num += _num
     return _sum/_num
 
-if __name__ == '__main__':
+def calculate_test_result():
     csv_path = sub_path + 'LSTM_MAPE_list.csv'
 
     csv_list = open(csv_path).readlines()
@@ -50,3 +52,41 @@ if __name__ == '__main__':
         mape = return_mape(predicted, temp_test_label)
         print('District: '+str(idx+1)+' '+ entry_list[0] + '   %f') % mape
     print('Overall mape: %f') % (mape_sum/mape_num)
+
+
+def choose_best_model():
+    global result_list, sub_path1
+    best_mape_list = []
+    for model_idx in range(66):
+        mape_list = []
+        model_list = []
+        activator_list = []
+        test_data, test_label = load_test_data(sub_path1)
+        temp_test_label = test_label[..., model_idx]
+        for path_idx in range(11):
+            result_sub_path = MODEL_OUT_PATH + result_list[path_idx]+'/'
+            csv_path = result_sub_path + 'LSTM_MAPE_list.csv'
+            line = linecache.getline(csv_path, model_idx+1)
+            line = line.strip('\n')
+            line_list = line.split(',')
+            activator = line_list[1]
+            model = initial_lstm_model(activator)
+            model_path = result_sub_path + 'model_district_' + str(model_idx + 1) + '.h5'
+            model.load_weights(model_path)
+            predicted = model.predict(test_data)
+            mape = return_mape(predicted, temp_test_label)
+            # save all the result
+            activator_list.append(activator)
+            mape_list.append(mape)
+            model_list.append(model)
+        best_idx = mape_list.index(min(mape_list))
+        # print result
+        print('District: %d %f  '+result_list[best_idx]) %(model_idx+1, mape_list[best_idx])
+        best_mape_list.append([mape_list[best_idx]] + [activator_list[best_idx]])
+        best_model = model_list[best_idx]
+        best_model.save_weights('../../final_result/attempt1/' + 'model_district_' + str(model_idx + 1) + '.h5', overwrite=True)
+    write_list_to_csv(best_mape_list, '../../final_result/attempt1/'+'LSTM_MAPE_list.csv')
+
+if __name__ == '__main__':
+    choose_best_model()
+
