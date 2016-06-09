8f425e6cd6bed8509c6b2cbf7a49df59e6108945
harrysocool <harry_so_cool@live.com>
2016-06-09 22:27:38 +0100

    wrong mape calculating which cause the mape error to be 0.00.... fuck!

--- python_code/LSTM/LSTM.py
+++ python_code/LSTM/LSTM.py
@@ -7,18 +7,18 @@ Created on 16/6/6 21:32 2016
 import os
 import time
 import keras
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import ModelCheckpoint, Callback
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation, Dropout, TimeDistributedDense, Flatten
 from keras.layers.recurrent import LSTM
 from keras.regularizers import l2, activity_l2
-from extend_function import write_list_to_csv
+from extend_function import write_list_to_csv, save_test_csv
 from process_data import *
 
 seed = 5
-batch_size = 32
+batch_size = 500
 PARENT_IN_PATH = '../../processed_data/'
-MODEL_OUT_PATH = '../../result/attempt5'+'_batch_size_'+str(batch_size)+'/'
+MODEL_OUT_PATH = '../../result/attempt9'+'_batch_size_'+str(batch_size)+'/'
 train_path = PARENT_IN_PATH + 'didi_train_data.csv'
 label_path = PARENT_IN_PATH + 'didi_train_label.csv'
 mape_sum = 0
@@ -68,7 +68,7 @@ def initial_lstm_model(activator):
     model.add(Dropout(0.25))
     model.add(Activation(activator))
 
-    model.add(Dense(1, W_regularizer=l2(0.1), activity_regularizer=activity_l2(0.1)))
+    model.add(Dense(1))
     model.add(Activation(activator))
 
     optimizer = keras.optimizers.Adam(lr=0.2, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
@@ -77,46 +77,57 @@ def initial_lstm_model(activator):
     return model
 
 
-def train_model(model, x_train, y_train, x_test, y_test, district_id):
-    # model.fit(x_train, y_train, verbose=False, batch_size=32, nb_epoch=40)
+def train_model(model, x_train, y_train, district_id):
+    class ProcessControl(Callback):
+        def on_train_begin(self, logs={}):
+            self.losses = []
+
+        def on_epoch_begin(self, epoch, logs={}):
+            new_lr = np.float32(model.optimizer.lr.get_value() * 0.99)
+            model.optimizer.lr.set_value(new_lr)
+            # print new_lr
+
+    decay_lr = ProcessControl()
     checkpointer = ModelCheckpoint(MODEL_OUT_PATH+'model_district_'+str(district_id+1)+'.h5',
                                    verbose=1, save_best_only=True)
-    earlystopping = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10, verbose=1, mode='min')
-    model.fit(x_train, y_train, verbose=0, validation_data=(x_test, y_test), batch_size=batch_size,
-              nb_epoch=100, callbacks=[earlystopping, checkpointer])
+    earlystopping = keras.callbacks.EarlyStopping(monitor='val_loss', patience=15, verbose=1, mode='min')
+    model.fit(x_train, y_train, verbose=0, validation_split=0.1, batch_size=batch_size, shuffle=True,
+              nb_epoch=100, callbacks=[decay_lr, earlystopping, checkpointer])
+
 
 
 def return_mape(predict_result, true_result):
     global mape_sum, mape_num
-    predict_result1 = predict_result[0]
-    a = np.abs(true_result - predict_result1)
+    predict_result1 = predict_result.flatten()
+    true_result1 = true_result
+    a = np.abs(true_result1 - predict_result1)
     _sum = 0
     _num = 0
     for j in range(len(predict_result1)):
-        if true_result[j] != 0:
-            _sum = _sum + a[j] / true_result[j]
+        if true_result1[j] != 0:
+            _sum = _sum + a[j] / true_result1[j]
             _num += 1
     mape_sum += _sum
     mape_num += _num
     return _sum/_num
 
 
-def multi_model(x_train, y_train, x_test, y_test):
-    activator_list = ['linear', 'tanh']
+def multi_model(x_train, y_train, x_validate, y_validate):
+    activator_list = ['linear', 'tanh', 'sigmoid']
     mape_list = []
     for district_id in range(66):
         mape_entry = []
         model_list = []
         temp_x_train, temp_y_train = clean_data(x_train, y_train[..., district_id])
-        temp_x_test, temp_y_test = clean_data(x_test, y_test[..., district_id])
+        temp_x_validate, temp_y_validate = clean_data(x_validate, y_validate[..., district_id])
         start = time.time()
         for activator in activator_list:
             model = initial_lstm_model(activator=activator)
-            train_model(model, temp_x_train, temp_y_train, temp_x_test, temp_y_test, district_id)
+            train_model(model, temp_x_train, temp_y_train, district_id)
             model.load_weights(MODEL_OUT_PATH+'model_district_'+str(district_id+1)+'.h5')
-            predicted = model.predict(temp_x_test)
+            predicted = model.predict(temp_x_validate)
             model_list.append(model)
-            mape_entry.append(return_mape(predicted, temp_y_test))
+            mape_entry.append(return_mape(predicted, temp_y_validate))
         end = time.time()
         best_idx = mape_entry.index(min(mape_entry))
         best_model = model_list[best_idx]
@@ -137,11 +148,14 @@ if __name__ == '__main__':
     label_data_str = open(label_path)
     train_list_str = train_data_str.readlines()
     label_list_str = label_data_str.readlines()
-    (train_data, train_label), (test_data, test_label) = train_test_split(train_list_str, label_list_str,
-                                                                          seed=seed, test_size=0.2)
-    mape = multi_model(train_data, train_label, test_data, test_label)
+    (train_data, train_label), (validate_data, validate_label), (test_data, test_label) \
+        = train_data_split(train_list_str, label_list_str, seed=seed)
+    # save the test_data into the models directory
+    save_test_csv(MODEL_OUT_PATH, test_data, test_label)
+    mape = multi_model(train_data, train_label, validate_data, validate_label)
     out_path = MODEL_OUT_PATH + 'LSTM_MAPE_list.csv'
+    # save the validation MAPE for every model and overall MAPE
     write_list_to_csv(mape, out_path)
     ed_time = time.time()
-    print(' Overall Time: %.2f minutes' % ((st_time - ed_time) / 3600))
+    print(' Overall Time: %.2f hours' % ((ed_time - st_time) / 3600))
     print 'overall mape loss: %f\n' % (mape_sum / mape_num)
--- python_code/LSTM/process_data.py
+++ python_code/LSTM/process_data.py
@@ -8,7 +8,7 @@ import numpy as np
 import pandas as pd
 
 
-def _construct_Xdata(data):
+def _construct_xdata(data):
     """
     data should be pd.DataFrame()
     """
@@ -47,29 +47,40 @@ def clean_data(x, y):
     y_out = y[non_zero_idx]
     return x_out, y_out
 
-def train_test_split(train_list, label_list, seed, test_size=0.2):
+def train_data_split(train_list, label_list, seed, train_size=0.7, test_size=0.1):
     """
     This just splits data to training and testing parts
     """
     data_ = range(len(train_list))
     label_ = range(len(train_list))
     shuffle_idx = range(len(train_list))
-    np.random.seed(641)
+    # np.random.seed(641)
     np.random.shuffle(shuffle_idx)
     for i in range(len(shuffle_idx)):
         idx_ = shuffle_idx[i]
-        data_[idx_] = train_list[idx_]
-        label_[idx_] = label_list[idx_]
+        data_[i] = train_list[idx_]
+        label_[i] = label_list[idx_]
 
-    ntrn = int(round(len(train_list) * (1 - test_size)))
+    idx1 = int(round(len(train_list) * train_size))
+    idx2 = int(round(len(train_list) * (1 - test_size)))
 
     train_data = pd.DataFrame(data_)
     label_data = pd.DataFrame(label_)
 
-    X_train = _construct_Xdata(train_data.iloc[0:ntrn])
-    y_train = _construct_ydata(label_data.iloc[0:ntrn])
-    X_test = _construct_Xdata(train_data.iloc[ntrn:])
-    y_test = _construct_ydata(label_data.iloc[ntrn:])
+    X_train = _construct_xdata(train_data.iloc[0:idx1])
+    y_train = _construct_ydata(label_data.iloc[0:idx1])
+    x_validate = _construct_xdata(train_data.iloc[idx1:idx2])
+    y_validate = _construct_ydata(label_data.iloc[idx1:idx2])
+    X_test = train_data.iloc[idx2:]
+    y_test = label_data.iloc[idx2:]
 
-    return (X_train, y_train), (X_test, y_test)
+    return (X_train, y_train), (x_validate, y_validate), (X_test, y_test)
+
+
+def load_test_data(path):
+    temp_test_xdata = pd.read_csv(path + 'test_data.csv')
+    temp_test_ydata = pd.read_csv(path + 'test_label.csv')
+    test_data = _construct_xdata(temp_test_xdata)
+    test_label = _construct_ydata(temp_test_ydata)
+    return test_data, test_label
 
